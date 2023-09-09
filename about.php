b01798831383df712fb76d1a13fe87e2299c4659
Shivhosamani <120409599+Shivhosamani@users.noreply.github.com>
2023-09-09 23:43:57 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,32 @@
+<div class="col-12">
+    <div class="row my-5 ">
+        <div class="col-md-5">
+            <div class="card card-outline card-maroon rounded-0 shadow">
+                <div class="card-header">
+                    <h4 class="card-title">Contact</h4>
+                </div>
+                <div class="card-body rounded-0">
+                    <dl>
+                        <dt class="text-muted"><i class="fa fa-envelope"></i> Email</dt>
+                        <dd class="pl-4">shivshankarhosamani9@gmail.com</dd>
+                        <dt class="text-muted"><i class="fa fa-phone"></i> Contact #</dt>
+                        <dd class="pl-4">9686612481 / 9901433108</dd>
+                        <dt class="text-muted"><i class="fa fa-map-marked-alt"></i> Location</dt>
+                        <dd class="pl-4"> Bangalore - 560004 </dd>
+                    </dl>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-7">
+            <div class="card rounded-0 card-outline card-maroon shadow" >
+                <div class="card-body rounded-0">
+                    <h2 class="text-center">About</h2>
+                    <center><hr class="bg-maroon border-maroon w-25 border-2"></center>
+                    <div>
+                        <?= file_get_contents("about_us.html") ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
